0c2b89b0c81e670e98cd23556cc90542e2b2c26b
geremachek <jonahgrongstad@gmail.com>
2019-09-02 18:48:10 -0500

    2

--- puck.go
+++ puck.go
@@ -94,7 +94,7 @@ func getSchemeIndex(scheme []Format, normal int, file string) (out int) {
 func dispFiles(screen tcell.Screen, scheme []Format, files []string, top int, bottom int, side int, dir int, normal int) {
 	_, height := screen.Size()
 	if len(files) == 0 {
-		addstr(screen, tcell.StyleDefault.Background(tcell.GetColor("#ff0000")).Bold(true), side, top, sizeText(screen, false, side, "There's nothing here!"))
+		addstr(screen, tcell.StyleDefault.Foreground(tcell.GetColor("#f40404")).Bold(true), side, top, sizeText(screen, false, side, "There's nothing here!"))
 	} else {
 		for i, elem := range files {
 			if i != height - (bottom) {
@@ -142,11 +142,31 @@ func dSelFile(screen tcell.Screen, y int, index int, side int, dir int, normal i
 	width, _ := screen.Size()
 	addstr(screen, tcell.StyleDefault, 0, y, rstrx(" ", width))
 	if isd(array[index]) {
-		addstr(screen, format2style(scheme[getSchemeIndex(scheme, dir, array[index])]), 0, y, sizeText(screen, true, side, array[index]))
+		addstr(screen, format2style(scheme[getSchemeIndex(scheme, dir, array[index])]), side, y, sizeText(screen, true, side, array[index]))
 	} else {
-		addstr(screen, format2style(scheme[getSchemeIndex(scheme, normal, array[index])]), 0, y, sizeText(screen, false, side, array[index]))
+		addstr(screen, format2style(scheme[getSchemeIndex(scheme, normal, array[index])]), side, y, sizeText(screen, false, side, array[index]))
+	}
+}
+
+func bar(screen tcell.Screen, text []string, schemes []Format, full tcell.Style, side int, top int, bottom int, location string) {
+	_, height := screen.Size()
+	var y int
+	var x int
+	if location == "bottom" {
+		y=height-bottom+1
+	} else if location == "top" {
+		y=top/2
+	}
+	for i, elem := range text {
+		if i == 0 {
+			addstr(screen, format2style(schemes[i]), x+side, y, elem)
+		} else {
+			addstr(screen, format2style(schemes[i]), x+side, y, " "+elem)
+		}
+		x += len(elem)
 	}
 
+	addstr(screen, full, x+side, y, rstrx(" ", width-x))
 }
 
 func main() {
@@ -158,7 +178,7 @@ func main() {
 		dir int
 		normal int
 		top int = 0
-		bottom int = 0
+		bottom int = 3
 		side int = 0
 	)
 	var (
@@ -170,6 +190,7 @@ func main() {
 	cwd, _ := os.Getwd()
 	mainList := getFiles(cwd)
 	selStyle := Format{"", "full", "#ffffff", "#878080", false, false, false, false}
+	barStyle := Format{"", "", "#ffffff", "#0b4741", false, false, false, false}
 	defScheme := []Format{ Format{"[dir]", "", "#94bff3", "", true, false, false, false}, Format{"[normal]", "", "", "", false, false, false, false}, Format{"jpg", "", "#ff00ff", "", false, false, false, false} }
 	for i, elem := range defScheme {
 		if elem.What == "[dir]" {
@@ -182,6 +203,7 @@ func main() {
 	}
 	dispFiles(s, defScheme, mainList, top, bottom, side, dir, normal)
 	selFile(s, selStyle, side, currY, mainList[currFile])
+	bar(s, []string{"Hello","World"}, []Format{barStyle, barStyle}, side, top, bottom, "bottom")
 	s.Show()
 
 	for {
@@ -193,7 +215,7 @@ func main() {
 					s.Fini()
 					fmt.Println(currY, currFile)
 					os.Exit(0)
-				} else if input.Key() == tcell.KeyDown {
+				} else if input.Key() == tcell.KeyDown && len(mainList) != 0 {
 					if currFile == len(mainList)-1 {
 						continue
 					} else if currY == (height-1)-bottom {
@@ -203,6 +225,7 @@ func main() {
 						s.Clear()
 						dispFiles(s, defScheme, mainList[b1:b2], top, bottom, side, dir, normal)
 						selFile(s, selStyle, side, currY, mainList[currFile])
+						bar(s, []string{"Hello"}, []Format{barStyle}, side, top, bottom, "bottom")
 						s.Show()
 					} else {
 						dSelFile(s, currY, currFile, side, dir, normal, mainList, defScheme)
@@ -211,7 +234,7 @@ func main() {
 						selFile(s, selStyle, side, currY, mainList[currFile])
 						s.Show()
 					}
-				} else if input.Key() == tcell.KeyUp {
+				} else if input.Key() == tcell.KeyUp && len(mainList) != 0 {
 					if currFile == 0 {
 						continue
 					} else if currY == top && currFile != 0 {
@@ -221,6 +244,7 @@ func main() {
 						s.Clear()
 						dispFiles(s, defScheme, mainList[b1:b2], top, bottom, side, dir, normal)
 						selFile(s, selStyle, side, currY, mainList[currFile])
+						bar(s, []string{"Hello"}, []Format{barStyle}, side, top, bottom, "bottom")
 						s.Show()
 					} else {
 						dSelFile(s, currY, currFile, side, dir, normal, mainList, defScheme)
@@ -229,7 +253,7 @@ func main() {
 						selFile(s, selStyle, side, currY, mainList[currFile])
 						s.Show()
 					}
-				} else if input.Key() == tcell.KeyRight {
+				} else if input.Key() == tcell.KeyRight && len(mainList) != 0 {
 					if isd(mainList[currFile]) {
 						os.Chdir(mainList[currFile])
 						cwd, _ = os.Getwd()
@@ -237,11 +261,24 @@ func main() {
 						b1, b2, currFile, currY = 0, (height - bottom)-top, 0, top
 						s.Clear()
 						dispFiles(s, defScheme, mainList, top, bottom, side, dir, normal)
-						selFile(s, selStyle, side, currY, mainList[currFile])
+						if len(mainList) != 0 {
+							selFile(s, selStyle, side, currY, mainList[currFile])
+						}
+						bar(s, []string{"Hello"}, []Format{barStyle}, side, top, bottom, "bottom")
 						s.Show()
 					} else {
 						continue
 					}
+				} else if input.Key() == tcell.KeyLeft {
+					os.Chdir("..")
+					cwd, _ = os.Getwd()
+					mainList = getFiles(cwd)
+					b1, b2, currFile, currY = 0, (height - bottom)-top, 0, top
+					s.Clear()
+					dispFiles(s, defScheme, mainList, top, bottom, side, dir, normal)
+					selFile(s, selStyle, side, currY, mainList[currFile])
+					bar(s, []string{"Hello"}, []Format{barStyle}, side, top, bottom, "bottom")
+					s.Show()
 				}
 		}
 	}
